91172b1fef427d8099a070a46ba3c5bb6c84bb3c
Jono Chang <j.g.chang@gmail.com>
2026-02-16 18:50:38 +1100

    add package.nix

---
+++ package.nix
@@ -0,0 +1,75 @@
+# This file goes in nixpkgs at: pkgs/by-name/un/untangle/package.nix
+#
+# To get the real cargoHash, set it to lib.fakeHash, run `nix-build -A untangle`,
+# and the error output will contain the correct hash.
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  cmake,
+  openssl,
+  libgit2,
+  zlib,
+  stdenv,
+  darwin,
+  testers,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "untangle";
+  version = "0.2.0";
+
+  src = fetchFromGitHub {
+    owner = "jonochang";
+    repo = "untangle";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-qjwglo9YNBelLq6wM0QOSspi63qoUyz128Xh1fVRJ50=";
+  };
+
+  cargoHash = lib.fakeHash;
+
+  nativeBuildInputs = [
+    pkg-config
+    cmake
+  ];
+
+  buildInputs =
+    [
+      openssl
+      libgit2
+      zlib
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.SystemConfiguration
+    ];
+
+  env = {
+    OPENSSL_NO_VENDOR = "1";
+    LIBGIT2_NO_VENDOR = "1";
+  };
+
+  # Integration tests require git repos and filesystem fixtures
+  checkFlags = [
+    "--skip=integration"
+  ];
+
+  passthru.tests.version = testers.testVersion {
+    package = finalAttrs.finalPackage;
+    command = "untangle --version";
+  };
+
+  meta = {
+    description = "Module-level dependency graph analyzer for Go, Python, Ruby, and Rust";
+    homepage = "https://github.com/jonochang/untangle";
+    changelog = "https://github.com/jonochang/untangle/blob/v${finalAttrs.version}/CHANGELOG.md";
+    license = with lib.licenses; [
+      mit
+      asl20
+    ];
+    maintainers = with lib.maintainers; [ jonochang ];
+    mainProgram = "untangle";
+    platforms = lib.platforms.unix;
+  };
+})
